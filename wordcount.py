c57203a3406ffa447820da7b6c62cadc755bfe98
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 12:48:01 -0700

    Added Flexibility

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,9 @@
 
 # put your code here.
 
+# Import sys module to pass the argument from the command line
+import sys
+
 # Create the dictionary WORDCOUNT
 wordcount = {}
 
@@ -16,18 +19,20 @@ def to_count(textfile):
     # Each time it is seen
 
     # Splits the poem into a list of words
+    # Right strips each word of excess spaces etc
     for line in data:
-        words = line.rstrip().split()
+        words = line.rstrip(' ,.?!"').split()
 
         # Iterate over the list of words
         for word in words:
+            word = word.lower()
             wordcount[word] = wordcount.get(word, 0) + 1
     
     # Print the key and value, i.e. word and its occurences in text 
-    for key, value in wordcount.items():
+    for key, value in sorted(wordcount.items()):
         print key, value 
 
-to_count('twain.txt')
+to_count(sys.argv[1])
 
 
 
